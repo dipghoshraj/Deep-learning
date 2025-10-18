7adfb3d9fbf28565582000583c71cd2b3e82fe53
dipghoshraj <dipghoshraj@gmail.com>
2025-10-19 05:13:15 +0530

    slm : build the flask application

---
+++ Application/application.py
@@ -0,0 +1,26 @@
+from generate import Slm
+
+slm = Slm()
+
+# print(slm.generate("I feel lonely and anxious sometimes because"))
+
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_text():
+    data = request.json
+    input_text = data.get('input_text', '')
+    max_new_tokens = data.get('max_new_tokens', 100)
+
+    generated_text = slm.generate(input_text, max_new_tokens=max_new_tokens)
+    return jsonify({'generated_text': generated_text})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ Application/generate.py
@@ -0,0 +1,35 @@
+from pymodel import GPT
+import torch
+
+
+TOKENIZER_PATH = "model/custom_tokenizer_mental.json"
+
+from transformers import PreTrainedTokenizerFast
+tok = PreTrainedTokenizerFast(tokenizer_file=TOKENIZER_PATH)
+vocab_size = tok.vocab_size
+
+d_model = context_length= 512
+n_heads = 4
+n_layers = 2
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+class Slm:
+    def __init__(self):
+        self.m = GPT(vocab_size=vocab_size, d_model=d_model, n_heads=n_heads, n_layers=n_layers, context_length=context_length).to(device)
+        checkpoint = torch.load("model/best_model.pt", map_location="cuda" if torch.cuda.is_available() else "cpu")
+        self.m.load_state_dict(checkpoint['model_state_dict'])
+
+
+    def generate(self, input_text, max_new_tokens=100):
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.m.to(device)
+        self.m.eval()
+        with torch.no_grad():
+            input_ids = torch.tensor(tok.encode(input_text), dtype=torch.long, device=device).unsqueeze(0)
+            output = self.m.generate(input_ids, max_new_tokens=max_new_tokens)
+            generated_text = tok.decode(output[0].tolist())
+        return generated_text
